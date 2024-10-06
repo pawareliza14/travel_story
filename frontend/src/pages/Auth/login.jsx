3b8c49a491477d09c099291e149ad8f10c3b39a6
Krantivarekar <krantivarekar5260@gmail.com>
2024-10-06 15:51:59 +0530

    frontend1

---
+++ frontend/src/pages/Auth/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='h-screen bg-cyan-50 overflow-hidden relative'>
+      
+    </div>
+  )
+}
+
+export default login
