b311823e69e142d47fd398859ac0fbc788c380aa
Krantivarekar <krantivarekar5260@gmail.com>
2024-10-07 22:38:18 +0530

    frontend2

--- frontend/src/components/Input/Navbar.jsx
+++ frontend/src/components/Input/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import LOGO from '../assets/images/travel-icon.png'
+// import LOGO from '../assets/images/travel-icon.png'
 const Navbar = () => {
   return (
-    <div className=''>
+    <div className='bg-white flex items center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
       <img src={LOGO} alt='travel story' className='h-9'/>
     </div>
   )
--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import Navbar from '../../components/Input/Navbar'
+import { useNavigate } from 'react-router-dom'
 const Home = () => {
+  const navigate =useNavigate()
+
+  const [userInfo, setUserInfo] = useState(null)
+
+  //get user info
+  const getUserInfo= async()=>{
+    try{
+
+      const response=await axiosInstance.get("/get-user");
+
+      if(response.data && response.data.user)
+      {
+        //set user info if data exists
+        setUserInfo(response.data.user);
+      }
+
+    }catch(error){
+      if(error.response.status===401){
+        //clear storage if unauthorized
+        localStorage.clear();
+        navigate("/login");   //redirect to login
+      }
+    }
+  };
+
   return (
     <div>
       <Navbar/>
